73674a1ae9408e65cdaa075fc091bb6a6a6e7376
Mihail Yordanov <mialskywalker@gmail.com>
2025-09-14 18:52:35 +0300

    all done until the Create Window Instance part

--- LearningDirectX12/main.cpp
+++ LearningDirectX12/main.cpp
@@ -48,6 +48,88 @@ uint32_t g_ClientHeight = 720;
 // Set to true once DX12 objects have been initialized
 bool g_IsInitialized = false;
 
+// Window handle
+HWND g_hWnd;
+//Window rectangle (fullscreen state)
+RECT g_WindowRect;
+
+// DirectX 12 Objects
+ComPtr<ID3D12Device2> g_Device;
+ComPtr<ID3D12CommandQueue> g_CommandQueue;
+ComPtr<IDXGISwapChain4> g_SwapChain;
+ComPtr<ID3D12Resource> g_BackBuffers[g_NumFrames];
+ComPtr<ID3D12GraphicsCommandList> g_CommandList;
+ComPtr<ID3D12CommandAllocator> g_CommandAllocators[g_NumFrames];
+ComPtr<ID3D12DescriptorHeap> g_RTVDescriptorHeap;
+UINT g_RTVDescriptorSize;
+UINT g_CurrentBackBufferIndex;
+
+// Synchronization objects
+ComPtr<ID3D12Fence> g_Fence;
+uint64_t g_FenceValue = 0;
+uint64_t g_FrameFenceValues[g_NumFrames] = {};
+HANDLE g_FenceEvent;
+
+// V-Sync enabled by default (toggle with the 'V' key)
+bool g_VSync = true;
+bool g_TearingSupported = false;
+// Windowed mode by default (toggle with Alt+Enter or F11)
+bool g_Fullscreen = false;
+
+// Window callback function
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+void ParseCommandLineArguments() {
+	int argc;
+	wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+
+	for (size_t i = 0; i < argc; ++i) {
+		if (::wcscmp(argv[i], L"-w") == 0 || ::wcscmp(argv[i], L"--width") == 0) {
+			g_ClientWidth = ::wcstol(argv[++i], nullptr, 10);
+		}
+		if (::wcscmp(argv[i], L"-h") == 0 || ::wcscmp(argv[i], L"--height") == 0) {
+			g_ClientHeight = ::wcstol(argv[++i], nullptr, 10);
+		}
+		if (::wcscmp(argv[i], L"-warp") == 0 || ::wcscmp(argv[i], L"--warp") == 0) {
+			g_UseWarp = true;
+		}
+	}
+
+	// Free memory allocated by CommandLineToArgvW
+	::LocalFree(argv);
+}
+
+void EnableDebugLayer() {
+#if defined(_DEBUG)
+	// Always enable debug layer before anything DX12 related
+	ComPtr<ID3D12Debug> debugInterface;
+	ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+	debugInterface->EnableDebugLayer();
+#endif
+}
+
+void RegisterWindowClass(HINSTANCE hInst, const wchar_t* windowClassName) {
+	// Register window class (render window)
+	WNDCLASSEXW windowClass = { };
+
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = &WndProc;
+	windowClass.cbClsExtra = 0;
+	windowClass.cbWndExtra = 0;
+	windowClass.hInstance = hInst;
+	windowClass.hIcon = ::LoadIcon(hInst, NULL);
+	windowClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	windowClass.lpszMenuName = NULL;
+	windowClass.lpszClassName = windowClassName;
+	windowClass.hIconSm = ::LoadIcon(hInst, NULL);
+
+	static ATOM atom = ::RegisterClassExW(&windowClass);
+	assert(atom > 0);
+}
+
+
 
 int main() {
 	return 0;
